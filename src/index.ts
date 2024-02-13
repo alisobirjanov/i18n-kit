667348159c6f4877948418a074b71c0e0de69fec
Alimardon <alisobirzanov29@gmail.com>
2024-02-13 19:25:41 +0500

    refactor: splitting logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'node:path'
+import { cwd } from 'node:process'
 import type { Plugin } from 'vite'
+import { normalizePath } from 'vite'
 import MagicString from 'magic-string'
-import { variationsChars } from './utils'
+import { charCombinations, deepFlatten } from './utils'
 
 const regexp = /\$t\(\s*(["'\`])(.*?)\1\s*(\,.*?)?\)/gm
 
@@ -10,82 +12,67 @@ export default function vitePluginI18n(options: any = {}): Plugin {
     locales = './locales',
   } = options
 
-  const localesDir = resolve(process.cwd(), locales).split('\\').join('-')
+  const localesDir = normalizePath(resolve(cwd(), locales))
 
   const localesMap = new Map()
+  const nextCombination = charCombinations()
 
-  const hashFn = variationsChars()
+  const hashFn = (key: string) => {
+    let hash = localesMap.get(key)
+    if (!hash) {
+      hash = nextCombination()
+      localesMap.set(key, hash)
+    }
+    return hash
+  }
 
   return {
     enforce: 'pre',
     name: 'vite-plugin-i18n',
     transform(code: string, id: string) {
-      id = id.split('/').join('-')
-      if (id.startsWith(localesDir) && id.endsWith('.json')) {
-        try {
-          const json = deepFlatten(JSON.parse(code))
-          const result = Object.entries(json).reduce((acc, [key, value]) => {
-            let hash = localesMap.get(key)
-            if (!hash) {
-              hash = hashFn()
-              localesMap.set(key, hash)
-            }
-            acc[hash] = value
-            return acc
-          }, {})
-          console.log(1)
-          return {
-            code: JSON.stringify(result),
-          }
-        }
-        catch (err) {
-          console.log(err)
-        }
-      }
+      if (id.startsWith(localesDir) && id.endsWith('.json'))
+        return transformLocalesJson(code, hashFn)
+
       const matches = [...code.matchAll(regexp)]
+
       if (!matches.length)
         return
 
-      const s = new MagicString(code)
-
-      for (const match of matches) {
-        const key = match[2]
-        const hash = localesMap.get(key)
-        console.log(key, hash, localesMap.keys())
-        if (hash) {
-          const start = match.index!
-          s.overwrite(start + 1, start + key.length - 1, hash)
-        }
-      }
-      console.log(2)
-      return {
-        code: s.toString(),
-      }
+      return transformMatchesMessages(code, matches, hashFn)
     },
   }
 }
 
-interface Locales {
-  [key: string]: string | Locales
-}
+function transformLocalesJson(code: string, hashFn: (key: string) => string) {
+  try {
+    const json = deepFlatten(JSON.parse(code))
 
-function deepFlatten(locales: Locales) {
-  const map = {}
-  const objEntries = [...Object.entries(locales)]
-
-  for (let i = 0; i < objEntries.length; i++) {
-    const [key, value] = objEntries[i]
-
-    if (typeof value === 'string') {
-      map[key] = value
-    }
-    else {
-      // @ts-expect-error
-      objEntries.push(...Object.entries(value).map(([k, v]) => {
-        return [`${key}.${k}`, v]
-      }))
+    const result = Object.entries(json).reduce((acc: Record<string, string>, [key, value]) => {
+      const hash = hashFn(key)
+      acc[hash] = value
+      return acc
+    }, {})
+    return {
+      code: JSON.stringify(result),
     }
   }
+  catch (err) {
+    console.log(err)
+  }
+}
 
-  return map
+function transformMatchesMessages(code: string, matches: any[], hashFn: (key: string) => string) {
+  const s = new MagicString(code)
+
+  for (const match of matches) {
+    const key = match[2]
+    const hash = hashFn(key)
+    const start = match.index!
+    const str = match[0].replace(key, hash)
+    s.overwrite(start, start + match[0].length, str)
+  }
+
+  return {
+    code: s.toString(),
+  }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -26,3 +26,27 @@ export function charCombinations(chars: string = defaultChars) {
     return combination.map(i => chars[i]).join('')
   }
 }
+
+interface Locales {
+  [key: string]: string | Locales
+}
+
+export function deepFlatten(locales: Locales) {
+  const map: Record<string, string> = {}
+  const objEntries = [...Object.entries(locales)]
+
+  for (let i = 0; i < objEntries.length; i++) {
+    const [key, value] = objEntries[i]
+
+    if (typeof value === 'string') {
+      map[key] = value
+    }
+    else {
+      objEntries.push(...Object.entries(value).map(([k, v]) => {
+        return [`${key}.${k}`, v]
+      }) as [string, string | Locales][])
+    }
+  }
+
+  return map
+}
