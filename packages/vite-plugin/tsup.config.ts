891e8119ff21e81ed71f968f36c8a598dc888571
Alimardon <alisobirzanov29@gmail.com>
2024-02-27 01:32:18 +0500

    chore: add external deps

--- packages/vite-plugin/tsup.config.ts
+++ packages/vite-plugin/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
-  external: ['node:process', 'node:path', 'vite', 'magic-string'],
+  external: ['node:process', 'node:path', 'node:fs', 'vite', 'magic-string', 'fast-glob'],
   format: ['esm', 'cjs'],
   dts: true,
 })
