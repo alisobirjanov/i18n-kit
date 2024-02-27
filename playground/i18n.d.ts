12f0c470d92b18285ee055345dd5716afe628cbf
Alimardon <alisobirzanov29@gmail.com>
2024-02-28 02:11:49 +0500

    chore: test hmr on new message file

--- playground/i18n.d.ts
+++ playground/i18n.d.ts
@@ -1,15 +1,17 @@
 declare module 'virtual:i18n-kit' {
+  import en from 'D:/open-source/ts-starter/playground/locales/en.json'
   import ru from 'D:/open-source/ts-starter/playground/locales/ru.json'
-	import uz from 'D:/open-source/ts-starter/playground/locales/uz.json'
+  import uz from 'D:/open-source/ts-starter/playground/locales/uz.json'
 
   declare module '@i18n-kit/vue' {
     interface Register {
       messages: {
+        en: typeof import('D:/open-source/ts-starter/playground/locales/en.json')
         ru: typeof import('D:/open-source/ts-starter/playground/locales/ru.json')
-				uz: typeof import('D:/open-source/ts-starter/playground/locales/uz.json')
+        uz: typeof import('D:/open-source/ts-starter/playground/locales/uz.json')
       }
     }
   }
 
-  export default { ru, uz }
+  export default { en, ru, uz }
 }
