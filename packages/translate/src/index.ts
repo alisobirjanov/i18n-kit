cb2abc1851e570b8549a078803c971767772ca5e
Alimardon <alisobirzanov29@gmail.com>
2024-03-09 01:20:10 +0500

    refactor(translate): translate fn

--- packages/translate/src/index.ts
+++ packages/translate/src/index.ts
@@ -1,22 +1,62 @@
 import { readFileSync, writeFileSync } from 'node:fs'
+import { parse, resolve } from 'node:path'
+import { cwd } from 'node:process'
+import type { ParsedPath } from 'node:path'
 import _translate from 'translate'
+import fg from 'fast-glob'
+import normalizePath from 'normalize-path'
 
-type Obj = Record<string, string | object>
+const root = cwd()
 
-function toJSON(path) {
-  path = readFileSync(`${path.dir}/${path.base}`, 'utf8')
-  return JSON.parse(path || '{}')
+interface Options {
+  localesPath?: string
+  from: string
+  key?: string
+  engine?: 'google' | 'deepl' | 'libre' | 'yandex'
 }
 
-export function translate(from, to) {
-  from = toJSON(from)
-  to.forEach(async (path) => {
-    const result = await translateDeep(from, toJSON(path), from.name, path.name)
+export function translate(options: Options) {
+  const {
+    localesPath = './locales',
+    from,
+    key,
+    engine,
+  } = options
+
+  if (key) {
+    // @ts-expect-error
+    _translate.key = key
+  }
+  if (engine) {
+    // @ts-expect-error
+    _translate.engine = engine
+  }
+
+  const glob = `${normalizePath(resolve(root, localesPath))}/*.json`
+
+  const localesParsedPaths = fg.sync(glob).map(parse)
+
+  const fromLocalePath = localesParsedPaths.find(i => i.name === from)
+  if (!fromLocalePath) {
+    // TODO: log error message
+    console.log('Error')
+    return
+  }
+  const toLocalesPaths = localesParsedPaths.filter(i => i.name !== from)
+
+  const fromMessages = toJSON(fromLocalePath)
+  toLocalesPaths.forEach(async (path) => {
+    const result = await translateDeep(fromMessages, toJSON(path), fromLocalePath.name, path.name)
     writeFileSync(`${path.dir}/${path.base}`, JSON.stringify(result, null, 2))
   })
 }
 
-async function translateDeep(from: Obj, to: Obj, fromLang: string, toLang: string) {
+function toJSON(path: ParsedPath) {
+  const msg = readFileSync(`${path.dir}/${path.base}`, 'utf8')
+  return JSON.parse(msg || '{}')
+}
+
+async function translateDeep(from: any, to: any, fromLang: string, toLang: string) {
   const msg = Object.entries(from).map((item) => {
     return {
       current: to,
@@ -37,7 +77,7 @@ async function translateDeep(from: Obj, to: Obj, fromLang: string, toLang: strin
       if (!current[key])
         current[key] = {}
 
-      Object.entries(value).forEach((item) => {
+      Object.entries(value as any).forEach((item) => {
         msg.push({
           current: current[key],
           item,
