dc85f61656dcc9b0bcb30732e455738d163e03dd
Alimardon <alisobirzanov29@gmail.com>
2024-03-10 15:48:30 +0500

    refactor(translate): refactor

--- packages/translate/src/index.ts
+++ packages/translate/src/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from 'node:fs'
 import { parse, resolve } from 'node:path'
-import { cwd } from 'node:process'
+import { cwd, exit } from 'node:process'
 import type { ParsedPath } from 'node:path'
 import _translate from 'translate'
 import fg from 'fast-glob'
@@ -9,15 +9,15 @@ import normalizePath from 'normalize-path'
 const root = cwd()
 
 interface Options {
-  localesPath?: string
-  from: string
+  locales?: string
+  from?: string
   key?: string
   engine?: 'google' | 'deepl' | 'libre' | 'yandex'
 }
 
-export function translate(options: Options) {
-  const {
-    localesPath = './locales',
+export async function translate(options: Options) {
+  let {
+    locales = './locales',
     from,
     key,
     engine,
@@ -32,42 +32,71 @@ export function translate(options: Options) {
     _translate.engine = engine
   }
 
-  const glob = `${normalizePath(resolve(root, localesPath))}/*.json`
+  const glob = `${normalizePath(resolve(root, locales))}/*.json`
 
   const localesParsedPaths = fg.sync(glob).map(parse)
 
+  if (!localesParsedPaths.length) {
+    console.log(`Error: Messages is not detect from ${normalizePath(locales)}/*.json`)
+    exit(1)
+  }
+
+  if (!from)
+    from = localesParsedPaths[0].name
+
   const fromLocalePath = localesParsedPaths.find(i => i.name === from)
   if (!fromLocalePath) {
-    // TODO: log error message
-    console.log('Error')
-    return
+    console.log(`Error: From messages '${from}.json' is not defined`)
+    exit(1)
   }
+
   const toLocalesPaths = localesParsedPaths.filter(i => i.name !== from)
 
-  const fromMessages = toJSON(fromLocalePath)
-  toLocalesPaths.forEach(async (path) => {
-    const result = await translateDeep(fromMessages, toJSON(path), fromLocalePath.name, path.name)
+  const fromMessages = readMessages(fromLocalePath)
+
+  console.log(`From Messages: ${fromLocalePath.base}\n`)
+  console.log('Translate completed:')
+
+  for (const path of toLocalesPaths) {
+    const messages = readMessages(path)
+    const result = await deepTranslateObjectValues(
+      fromMessages,
+      messages,
+      fromLocalePath.name,
+      path.name,
+    )
     writeFileSync(`${path.dir}/${path.base}`, JSON.stringify(result, null, 2))
-  })
+    console.log(`\t${path.base}`)
+  }
 }
 
-function toJSON(path: ParsedPath) {
-  const msg = readFileSync(`${path.dir}/${path.base}`, 'utf8')
-  return JSON.parse(msg || '{}')
+function readMessages(path: ParsedPath) {
+  try {
+    const msg = readFileSync(`${path.dir}/${path.base}`, 'utf8')
+    return JSON.parse(msg || '{}')
+  }
+  catch (err: any) {
+    console.log(`Error: ${err.messages}`)
+    exit(1)
+  }
 }
 
-async function translateDeep(from: any, to: any, fromLang: string, toLang: string) {
-  const msg = Object.entries(from).map((item) => {
+interface Messages {
+  [key: string]: string | Messages
+}
+
+async function deepTranslateObjectValues(from: Messages, to: Messages, fromLang: string, toLang: string) {
+  const stack = Object.entries(from).map((messages) => {
     return {
       current: to,
-      item,
+      messages,
     }
   })
 
-  for (let i = 0; i < msg.length; i++) {
-    const { current, item } = msg[i]
+  for (let i = 0; i < stack.length; i++) {
+    const { current, messages } = stack[i]
 
-    const [key, value] = item
+    const [key, value] = messages
 
     if (typeof value === 'string') {
       if (!current[key])
@@ -77,10 +106,10 @@ async function translateDeep(from: any, to: any, fromLang: string, toLang: strin
       if (!current[key])
         current[key] = {}
 
-      Object.entries(value as any).forEach((item) => {
-        msg.push({
+      Object.entries(value).forEach((messages) => {
+        stack.push({
           current: current[key],
-          item,
+          messages,
         } as any)
       })
     }
